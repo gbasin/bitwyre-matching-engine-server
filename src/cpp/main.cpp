bf1c0e0bce2807fbc9b78ff775445eabb1973e49
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2018-05-13 18:12:26 -0400

    adding RPC connection method

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -3,8 +3,10 @@
 using namespace Pistache;
 
 struct HelloHandler : public Http::Handler {
-    void onRequest(const Http::Request& req, Http::ResponseWriter response) {
-        response.send(Http::Code::Ok, "You are connected to bitwyre technologies server");
+    HTTP_PROTOTYPE(HelloHandler)
+
+    void onRequest(const Http::Request& request, Http::ResponseWriter writer) {
+        writer.send(Http::Code::Ok, "Hello, World!");
     }
 };
 
